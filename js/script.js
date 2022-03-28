a53f5e72baca9c79e7875cce70cf129691e3147a
Sergey Samsonov <samsonov199625@gmail.com>
2022-03-28 16:16:07 +0300

    add cycles

--- js/script.js
+++ js/script.js
@@ -10,13 +10,54 @@ const personalMovieDB = {
     private: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов?"),
-      b = prompt("На сколько оцените его?"), 
-      c = prompt("Один из последних просмотренных фильмов?"),
-      d = prompt("На сколько оцените его?");
+//>>>>>>for<<<<<<
 
-//Добавление свойств в объект через квадратные скобки 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt("Один из последних просмотренных фильмов?"),
+//     b = prompt("На сколько оцените его?");
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50){
+//         personalMovieDB.movies[a] = b;
+//     } else {
+//         i--;
+//     }
+  
+// }
 
-console.log(personalMovieDB);
+//>>>>>do while<<<<<<
+
+// do {
+//     let i = 0;
+//     const a = prompt("Один из последних просмотренных фильмов?"),
+//     b = prompt("На сколько оцените его?");
+//     i++;
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//     } else {
+//         i--;
+//     }
+// } while (i <= 2);
+
+let i = 0;
+while(i < 2) {
+    const a = prompt("Один из последних просмотренных фильмов?"),
+    b = prompt("На сколько оцените его?");
+    i++;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+            } else {
+                i--;
+            }
+}
+
+
+if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+    console.log('Вы киноман');
+} else if (personalMovieDB.count < 10){
+    console.log('Просмотрено довольно мало фильмов');
+} else {
+    console.log('Произошла ошибка');
+}
+
+
